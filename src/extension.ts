8d96ed574c65ebd675333ff93c8536b7155e338d
linhx <nguyendinhlinhx@gmail.com>
2024-01-12 11:36:06 +0700

    feat: support windows cmd

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,24 @@
 import * as vscode from 'vscode';
-import cp from 'child_process'; 
+import cp from 'child_process';
 
 export function activate(context: vscode.ExtensionContext) {
   const command = 'openInOsTerminal.open';
-  const openTerminalCommand = process.platform === 'win32' ? 'start cmd' : 'x-terminal-emulator';
-  
+  const openTerminalCommand =
+    process.platform === 'win32' ? 'start cmd' : 'x-terminal-emulator';
+
   const commandHandler = (val: any) => {
-    const subprocess = cp.spawn(openTerminalCommand, {cwd: val.path, detached: true, stdio: 'ignore'});
+    const subprocess = cp.spawn(openTerminalCommand, {
+      cwd: val.fsPath,
+      detached: true,
+      shell: true,
+      stdio: 'ignore',
+    });
     subprocess.unref();
   };
 
-  context.subscriptions.push(vscode.commands.registerCommand(command, commandHandler));
+  context.subscriptions.push(
+    vscode.commands.registerCommand(command, commandHandler)
+  );
 }
 
 export function deactivate() {}
