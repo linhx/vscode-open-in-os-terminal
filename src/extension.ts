ebaff4056afde306323cb6dff95f3b618a072cc0
linhx <nguyendinhlinhx@gmail.com>
2024-03-08 17:17:39 +0700

    fix: temporary don't allow windows to open the dir if the selected item is a file

--- src/extension.ts
+++ src/extension.ts
@@ -5,12 +5,17 @@ import * as path from 'path';
 
 export function activate(context: vscode.ExtensionContext) {
   const command = 'openInOsTerminal.open';
-  const openTerminalCommand =
-    process.platform === 'win32' ? 'start cmd' : 'x-terminal-emulator';
+  const isWindows = process.platform === 'win32';
+  const openTerminalCommand = isWindows ? 'start cmd' : 'x-terminal-emulator';
 
   const commandHandler = (val: any) => {
-    const stat = fs.statSync(val.fsPath);
-    const dir = stat.isFile() ? path.dirname(val.fsPath) : val.fsPath;
+    let dir;
+    if (isWindows) {
+      dir = val.fsPath;
+    } else {
+      const stat = fs.statSync(val.fsPath);
+      dir = stat.isFile() ? path.dirname(val.fsPath) : val.fsPath;
+    }
     const subprocess = cp.spawn(openTerminalCommand, {
       cwd: dir,
       detached: true,
